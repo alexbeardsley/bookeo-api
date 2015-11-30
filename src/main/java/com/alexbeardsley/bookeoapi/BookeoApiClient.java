6d3fda296cb29257f855323a391bf1b08c92ac41
Alex Beardsley <alex@alexbeardsley.com>
2015-11-30 12:32:50 -0500

    updating to use a snapshot release of the swagger codegen because a bug in it was causing POSTS to fail (https://github.com/swagger-api/swagger-codegen/issues/1527)

--- src/main/java/com/alexbeardsley/bookeoapi/BookeoApiClient.java
+++ src/main/java/com/alexbeardsley/bookeoapi/BookeoApiClient.java
@@ -1,31 +1,18 @@
 package com.alexbeardsley.bookeoapi;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
 import com.bookeo.api.request.ApiClient;
-import com.bookeo.api.request.ApiException;
-import com.bookeo.api.request.Pair;
-import com.bookeo.api.request.TypeRef;
-import com.bookeo.api.request.auth.ApiKeyAuth;
-import com.bookeo.api.request.auth.Authentication;
+
+import java.text.SimpleDateFormat;
 
 public class BookeoApiClient extends ApiClient {
-	private static final String BASE_URL = "https://api.bookeo.com/v2";
-	private static final String BOOKEO_SECRET_KEY_HEADER = "X-Bookeo-secretKey";
-	private static final String BOOKEO_API_KEY_HEADER = "X-Bookeo-apiKey";
+	protected final String BASE_URL = "https://api.bookeo.com/v2";
+	protected final String BOOKEO_SECRET_KEY_HEADER = "X-Bookeo-secretKey";
+	protected final String BOOKEO_API_KEY_HEADER = "X-Bookeo-apiKey";
 	
 	public BookeoApiClient(String secretKey, String apiKey) {
 		super.setBasePath(BASE_URL);
 		super.addDefaultHeader(BOOKEO_SECRET_KEY_HEADER, secretKey);
 		super.addDefaultHeader(BOOKEO_API_KEY_HEADER, apiKey);
-	}
-	
-	@Override
-	public String formatDate(Date date) {
-		return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").format(date);
+		super.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX"));
 	}
 }
